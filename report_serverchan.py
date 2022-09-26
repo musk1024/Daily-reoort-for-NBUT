27083ce9a20d3c4592c0426c30bad536220db2ee
GrandDuke1106 <98164810+GrandDuke1106@users.noreply.github.com>
2022-09-26 22:16:51 +0800

    Update report_serverchan.py

--- report_serverchan.py
+++ report_serverchan.py
@@ -1,9 +1,49 @@
 import json
 import requests
 import time
+import smtplib
+from email.mime.text import MIMEText
 
 least = "2022-09-23" #最近一次核酸检测时间
-api_key = "" #server酱api_key
+
+def email_send(news):
+    '''
+    邮件发送
+    :param news: 发送的消息
+    :return: 
+    '''
+    msg_from = ''  # 发送方邮箱
+    passwd = ''  # 填入发送方邮箱的授权码授权码！！！
+
+    msg_to = ['']  # 收件人邮箱
+    # msg_to = ['***@163.com','***@126.com']  # 多个收件人邮箱
+
+    subject = "nbut健康打卡"  # 主题
+    content = news  # 内容
+
+    msg = MIMEText(content)
+
+   
+    msg['Subject'] = subject
+
+   
+    msg['From'] = msg_from
+
+    try:
+        # 通过ssl方式发送，服务器地址，端口，qq和网易的有区别，注意修改。
+        s = smtplib.SMTP_SSL("smtp.example.com", 465)
+
+        # 登录到邮箱
+        s.login(msg_from, passwd)
+
+        # 发送邮件：发送方，收件方，要发送的消息
+        s.sendmail(msg_from, msg_to, msg.as_string())
+
+        print('发送成功')
+    except s.SMTPException as e:
+        print(e)
+    finally:
+        s.quit()
 
 def All():
     headers={'Host': 'form.nbut.edu.cn',
@@ -26,56 +66,42 @@ def All():
 
 
     text = repr({
-	    "formWid": "33cbf04e092b4d17aef3946f245d5cb4",
-	    "userId": "AM@stamp522",
-	    "dataMap": {
-		    "wid": "",
-		    "INPUT_KWYMPWZO": "NAME",
-		    "INPUT_KWYMPWZP": "COMPOSE",
-		    "INPUT_KWZRFKE3": "CLASS",
-		    "INPUT_KWZ02SZK": "",
-		    "INPUT_KWZ02SZL": "",
-		    "RADIO_KWYMPX0A": "是",
-		    "RADIO_KX369F35": "杭州湾校区",
-		    "RADIO_KX1T8ENX": "否",
-		    "RADIO_KX1T8ENY": "否",
-		    "RADIO_KWYMPX04": "绿",
-		    "RADIO_KWYMPWZT": "是",
-		    "RADIO_KWYMPWZU": "两针",
-		    "RADIO_KWYMPWZV": "否",
-		    "DATEPICKER_KWYW0UX4": "last",
-		    "RADIO_KWYMPWZX": "阴性",
-		    "LOCATION_KX7NAIQR": "浙江省宁波市慈溪市",
-		    "INPUT_KX14LP0P": "杭州湾汽车学院",
-		    "RADIO_L0IZ3481": "无",
-		    "INPUT_KX7NAIQS": "",
-		    "INPUT_KX7NAIQT": "",
-		    "INPUT_KWYMPWZR": "",
-		    "RADIO_KWYMPX03": ""
-	    },
-	    "commitDate": "date",
-	    "commitMonth": "month",
-	    "auditConfigWid": ""})
+	"formWid": "33cbf04e092b4d17aef3946f245d5cb4",
+	"userId": "AM@stamp522",
+	"dataMap": {
+		"wid": "",
+		"INPUT_KWYMPWZO": "NAME",
+		"INPUT_KWYMPWZP": "COMPOSE",
+		"INPUT_KWZRFKE3": "CLASS",
+		"INPUT_KWZ02SZK": "",
+		"INPUT_KWZ02SZL": "",
+		"RADIO_KWYMPX0A": "是",
+		"RADIO_KX369F35": "杭州湾校区",
+		"RADIO_KX1T8ENX": "否",
+		"RADIO_KX1T8ENY": "否",
+		"RADIO_KWYMPX04": "绿",
+		"RADIO_KWYMPWZT": "是",
+		"RADIO_KWYMPWZU": "两针",
+		"RADIO_KWYMPWZV": "否",
+		"DATEPICKER_KWYW0UX4": "last",
+		"RADIO_KWYMPWZX": "阴性",
+		"LOCATION_KX7NAIQR": "浙江省宁波市慈溪市",
+		"INPUT_KX14LP0P": "杭州湾汽车学院",
+		"RADIO_L0IZ3481": "无",
+		"INPUT_KX7NAIQS": "",
+		"INPUT_KX7NAIQT": "",
+		"INPUT_KWYMPWZR": "",
+		"RADIO_KWYMPX03": ""
+	},
+	"commitDate": "date",
+	"commitMonth": "month",
+	"auditConfigWid": ""})
     data = eval(text.replace('stamp',str(int(time.time()))).replace('date',time.strftime("%Y-%m-%d", time.localtime())).replace('month',time.strftime("%Y-%m", time.localtime())).replace('last',least))
     datas=json.dumps(data)
     r=requests.post("https://form.nbut.edu.cn/dfi/formData/saveFormSubmitData", data=datas, headers=headers)
+    news = r.text
+    email_send(news)
     print(r.text)
-    result = r.json()
-    if result.get('message') == "请求成功":
-        print("打卡成功")
-        if api_key:
-            message(api_key, result.get('message'), text)
-    else:
-        print("打卡失败，错误信息: ", r.json().get("message"))
-        if api_key:
-            message(api_key, result.get('message'), text)
-
-def message(key, title, body):
-    """
-    微信通知打卡结果
-    """
-    msg_url = "https://sc.ftqq.com/{}.send?text={}&desp={}".format(key, title, body)
-    requests.get(msg_url)
 
 if __name__ == '__main__':
     All()
