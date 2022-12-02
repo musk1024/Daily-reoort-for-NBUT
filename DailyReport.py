d96e86becfc9677dff4e67427843e72ea002ff26
musk-1024 <83929038+musk-1024@users.noreply.github.com>
2022-12-02 21:39:39 +0800

    demo

--- DailyReport.py
+++ DailyReport.py
@@ -21,7 +21,7 @@ def report(stamp_random):
         'Access-Control-Allow-Origin': '*',
         'Accept': 'application/json, text/plain, */*',
         'Access-Control-Allow-Credentials': 'true',
-        'Authentication': Authentication,
+        'Authentication': GlobalVariable.Authentication,
         'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36 NetType/WIFI MicroMessenger/7.0.20.1781(0x6700143B) WindowsWechat(0x6304051b)',
         'Origin': 'https://form.nbut.edu.cn',
         'Content-Type': 'application/json;charset=UTF-8',
@@ -34,8 +34,8 @@ def report(stamp_random):
         'Accept-Language': 'zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7'}
 
     print(headers)
-    text = repr(TEXT)
-    data = eval(text.replace('stamp',str(stamp_random)).replace('date',time.strftime("%Y-%m-%d", time.localtime())).replace('month',time.strftime("%Y-%m", time.localtime())).replace('last',least))
+    text = repr(GlobalVariable.TEXT)
+    data = eval(text.replace('stamp',str(stamp_random)).replace('date',time.strftime("%Y-%m-%d", time.localtime())).replace('month',time.strftime("%Y-%m", time.localtime())).replace('last',GlobalVariable.least))
     datas=json.dumps(data)
     r=requests.post("https://form.nbut.edu.cn/dfi/formData/saveFormSubmitData", data=datas, headers=headers)
     print(r.text)
@@ -45,9 +45,9 @@ def report(stamp_random):
         message(result.get('message'), text)
     else:
         print("打卡失败，错误信息: ", r.json().get("message"))
-        message(api_key, result.get('message'), text)
+        message(result.get('message'), text)
 
-def push(title,content):
+def message(title,content):
 
     if isinstance(content,str):
         markdownContent = content
