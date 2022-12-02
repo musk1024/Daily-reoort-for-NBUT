687cc683a7f614cb468ca8ba1e7ae87b31950767
musk-1024 <83929038+musk-1024@users.noreply.github.com>
2022-12-02 21:14:43 +0800

    demo

---
+++ DailyReport.py
@@ -0,0 +1,81 @@
+import json
+import requests
+import time
+import random
+import os
+import NoticePush
+import markdown
+import GlobalVariable
+
+def time_random():
+    start = int(round(time.time() * 1000))
+    t = random.randint(0, 3600000)  # 在一小时内随机取出
+    end = start + t
+    print(end)
+    return end,t
+
+def report(stamp_random):
+    headers={'Host': 'form.nbut.edu.cn',
+        'Connection': 'keep-alive',
+        'Content-Length': '871',
+        'Access-Control-Allow-Origin': '*',
+        'Accept': 'application/json, text/plain, */*',
+        'Access-Control-Allow-Credentials': 'true',
+        'Authentication': Authentication,
+        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36 NetType/WIFI MicroMessenger/7.0.20.1781(0x6700143B) WindowsWechat(0x6304051b)',
+        'Origin': 'https://form.nbut.edu.cn',
+        'Content-Type': 'application/json;charset=UTF-8',
+        'Cookie': 'JSESSIONID=BC15ED74EFE4EA3C2CA702F3C8350A53',
+        'Sec-Fetch-Site': 'same-origin',
+        'Sec-Fetch-Mode': 'cors',
+        'Sec-Fetch-Dest': 'empty',
+        'Referer': 'https://form.nbut.edu.cn/&state=STATE',
+        'Accept-Encoding': 'gzip, deflate, br',
+        'Accept-Language': 'zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7'}
+
+    print(headers)
+    text = repr(TEXT)
+    data = eval(text.replace('stamp',str(stamp_random)).replace('date',time.strftime("%Y-%m-%d", time.localtime())).replace('month',time.strftime("%Y-%m", time.localtime())).replace('last',least))
+    datas=json.dumps(data)
+    r=requests.post("https://form.nbut.edu.cn/dfi/formData/saveFormSubmitData", data=datas, headers=headers)
+    print(r.text)
+    result = r.json()
+    if result.get('message') == "请求成功":
+        print("打卡成功")
+        message(result.get('message'), text)
+    else:
+        print("打卡失败，错误信息: ", r.json().get("message"))
+        message(api_key, result.get('message'), text)
+
+def push(title,content):
+
+    if isinstance(content,str):
+        markdownContent = content
+        normalContent = content
+    else:
+        # mk模板
+        markdownContent = NoticeTemplate.markdownTemplate().format(**content)
+        # 普通模板
+        normalContent = NoticeTemplate.normalTemplate().format(**content)
+
+    NoticePush.server_push(title, markdownContent.replace("- ***", "```"))
+    NoticePush.push_plus(title, markdownContent)
+        # print("标题->", title)
+        # print("内容->\n", markdownContent)
+    NoticePush.Push_Deer(title,normalContent)
+    NoticePush.telegram_bot(title, normalContent)
+    NoticePush.bark(title, normalContent)
+    NoticePush.enterprise_wechat(title, normalContent)
+
+
+    # 信息输出测试
+    print("标题->", title)
+    # print("内容->\n", normalContent)
+
+
+if __name__ == '__main__':
+    stamp_random,t_random = time_random()
+    tt = int(t_random / 1000)
+    print(tt)
+    #time.sleep (tt)
+    report(stamp_random)
---
+++ GlobalVariable.py
@@ -0,0 +1,49 @@
+import os
+TEXT = repr({
+	    "formWid": "33cbf04e092b4d17aef3946f245d5cb4",
+	    "userId": "AM@stamp",
+	    "dataMap": {
+		    "wid": "",
+		    "INPUT_KWYMPWZO": "NAME",
+		    "INPUT_KWYMPWZP": "COMPOSE",
+		    "INPUT_KWZRFKE3": "CLASS",
+		    "INPUT_KWZ02SZK": "",
+		    "INPUT_KWZ02SZL": "",
+		    "RADIO_KWYMPX0A": "是",
+		    "RADIO_KX369F35": "杭州湾校区",
+		    "RADIO_KX1T8ENX": "否",
+		    "RADIO_KX1T8ENY": "否",
+		    "RADIO_KWYMPX04": "绿",
+		    "RADIO_KWYMPWZT": "是",
+		    "RADIO_KWYMPWZU": "两针",
+		    "RADIO_KWYMPWZV": "否",
+		    "DATEPICKER_KWYW0UX4": "last",
+		    "RADIO_KWYMPWZX": "阴性",
+		    "LOCATION_KX7NAIQR": "浙江省宁波市慈溪市",
+		    "INPUT_KX14LP0P": "杭州湾汽车学院",
+		    "RADIO_L0IZ3481": "无",
+		    "INPUT_KX7NAIQS": "",
+		    "INPUT_KX7NAIQT": "",
+		    "INPUT_KWYMPWZR": "",
+		    "RADIO_KWYMPX03": ""
+	    },
+	    "commitDate": "date",
+	    "commitMonth": "month",
+	    "auditConfigWid": ""})
+
+least = "2022-10-08" #最近一次核酸检测时间
+Authentication = os.environ.get("Authentication", "") # 必填 认证信息
+TEXT = os.environ.get("TEXT", "") # 必填 填报的内容
+SERVERPUSHKEY = os.environ.get("SERVERPUSHKEY", "")  # Server酱推送
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")  # Telegram推送服务Token
+TG_USER_ID = os.environ.get("TG_USER_ID", "")  # Telegram推送服务UserId
+BARK = os.environ.get("BARK", "")  # bark消息推送服务,自行搜索; secrets可填;形如jfjqxDx3xxxxxxxxSaK的字符串
+PUSHPLUS = os.environ.get("PUSHPLUS", "")  # PUSHPLUS消息推送Token
+PUSHDEERKEY = os.environ.get("PUSHDEERKEY", "") # PUSHDEER消息推送KEY
+ACCESSTOKEN = os.environ.get("ACCESSTOKEN", "")  # 企业微信access_token     获取地址:https://work.weixin.qq.com/api/doc/90000/90135/91039
+CORPID = os.environ.get("CORPID", "")  # 企业ID  （如果已经填写ACCESSTOKEN  则无需填写这个）
+CORPSECRET = os.environ.get("CORPSECRET", "")  # 应用的凭证密钥  （如果已经填写ACCESSTOKEN  则无需填写这个）
+TOUSER = os.environ.get("TOUSER", "@all")  # touser指定接收消息的成员  默认为全部
+AGENTID = os.environ.get("AGENTID", "")  # agentid企业应用的id
+THUMB_MEDIA_ID = os.environ.get("THUMB_MEDIA_ID", "") #企业微信素材库图片id
+AUTHOR = os.environ.get("AUTHOR", "") #企业微信文章作者
